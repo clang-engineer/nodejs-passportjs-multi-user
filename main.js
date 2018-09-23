50a8c9799910e8a4ff35024bb48adf5f950481a3
orez <yorez@naver.com>
2018-09-23 19:47:57 +0900

    list불러오기, router 분기 구현

--- main.js
+++ main.js
@@ -9,11 +9,20 @@ var app = http.createServer(function (request, response) {
     var title = queryData.id;
 
     if (pathname === '/') {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-
-            var template = `
-        <!DOCTYPE html>
-        <html>
+        if (queryData.id === undefined) {
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'WELCOME';
+                var description = 'node.js!!';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list += '</ul>';
+                var template = `
+            <!DOCTYPE html>
+            <html>
         <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,20 +31,50 @@ var app = http.createServer(function (request, response) {
         </head>
         <body>
         <h1><a href="/">WEB</a></h1>
-        <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
+        ${list}
         <h2>${title}</h2>
         <p>${description}</p>
         </body>
         </html>
         `
-            response.writeHead(200);
-            response.end(template);
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            fs.readdir('./data', function (error, filelist) {
+                var title = queryData.id;
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i++;
+                }
+                list += '</ul>';
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
 
-        });
+                    var template = `
+            <!DOCTYPE html>
+            <html>
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>WEB1- ${title}</title>
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+        </body>
+        </html>
+        `
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+
+        }
     } else {
         response.writeHead(404);
         response.end('not found');
