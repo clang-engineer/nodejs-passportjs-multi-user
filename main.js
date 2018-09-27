f41cb2d3a1cd4c0f50d1b986becf4cd37eab2c36
orez <yorez@naver.com>
2018-09-27 20:48:49 +0900

    라우터 - 주소체계변경

--- main.js
+++ main.js
@@ -31,36 +31,13 @@ app.get('/', function (request, response) {
         ${description}
         <img src="/images/light.jpg" style="width:300px; display:block; margin-top:10px;">
         `,
-        `<a href="/create">CREATE</a>`);
+        `<a href="/topic/create">CREATE</a>`);
     response.send(html);
 });
 
-app.get('/page/:pageId', function (request, response, next) {
-    var filterID = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filterID}`, 'utf8', function (err, description) {
-        if (err) {
-            next(err);
-        } else {
-
-            var title = request.params.pageId;
-            var sanitizeTitle = sanitizeHtml(title);
-            var sanitizeDescription = sanitizeHtml(description, { allowedTags: ['h1'] });
-            var list = template.List(request.list);
-            var html = template.HTML(sanitizeTitle, list, sanitizeDescription,
-                `<a href="/create">CREATE</a>
-                <a href="/update/${title}">UPDATE</a>
-                <form action="/delete_process" method="post">
-                <input type="hidden" name="id" value="${title}">
-                <input type="submit" value="delete">
-                </form>`);
-            response.send(html);
-        }
-    });
-});
-
-app.get('/create', function (request, response) {
+app.get('/topic/create', function (request, response) {
     var description = `
-            <form action="/create_process" method="post">
+            <form action="/topic/create_process" method="post">
             <p><input name="title" type="text" placeholder="title"></p>
             <p><textarea name="description" placeholder="description"></textarea></p>
             <p><input type="submit"></p>
@@ -68,60 +45,82 @@ app.get('/create', function (request, response) {
             `;
     var list = template.List(request.list);
     var html = template.HTML('CREATE', list, description,
-        `<a href="/create">CREATE</a>`);
+        `<a href="/topic/create">CREATE</a>`);
     response.writeHead(200);
     response.end(html);
 });
 
-app.post('/create_process', function (request, response) {
+app.post('/topic/create_process', function (request, response) {
     var post = request.body;
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.writeHead(302, { Location: `/page/${title}` });
+        response.writeHead(302, { Location: `/topic/${title}` });
         response.end();
     });
 });
 
-app.get('/update/:pageId', function (request, response) {
+app.get('/topic/update/:pageId', function (request, response) {
     var title = request.params.pageId;
     var list = template.List(request.list);
     var filterID = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filterID}`, 'utf8', function (err, description) {
         var html = template.HTML('UPDATE', list, `
-                <form action="/update_process" method="post">
+                <form action="/topic/update_process" method="post">
                 <input type="hidden" name="id" value="${title}">
                 <p><input name="title" type="text" placeholder="title" value="${title}"></p>
                 <p><textarea name="description" placeholder="description">${description}</textarea></p>
                 <p><input type="submit"></p>
                 </form>
                 `,
-            `<a href="/create">CREATE</a>`);
+            `<a href="/topic/create">CREATE</a>`);
         response.writeHead(200);
         response.end(html);
     });
 });
 
-app.post('/update_process', function (request, response) {
+app.post('/topic/update_process', function (request, response) {
     var post = request.body;
     var id = post.id;
     var title = post.title;
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function (error) {
         fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-            response.writeHead(302, { Location: `/page/${title}` });
+            response.writeHead(302, { Location: `/topic/${title}` });
             response.end();
         });
     });
 });
 
-app.post('/delete_process', function (request, response) {
+app.post('/topic/delete_process', function (request, response) {
     var post = request.body;
     var id = post.id;
     var filterID = path.parse(id).base;
     fs.unlink(`data/${filterID}`, function (error) {
         response.writeHead(302, { Location: `/` });
         response.end();
+    });
+});
+
+app.get('/topic/:pageId', function (request, response, next) {
+    var filterID = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filterID}`, 'utf8', function (err, description) {
+        if (err) {
+            next(err);
+        } else {
+            var title = request.params.pageId;
+            var sanitizeTitle = sanitizeHtml(title);
+            var sanitizeDescription = sanitizeHtml(description, { allowedTags: ['h1'] });
+            var list = template.List(request.list);
+            var html = template.HTML(sanitizeTitle, list, sanitizeDescription,
+                `<a href="/topic/create">CREATE</a>
+                <a href="/topic/update/${title}">UPDATE</a>
+                <form action="/topic/delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+                </form>`);
+            response.send(html);
+        }
     });
 });
 
