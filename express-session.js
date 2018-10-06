5b034bccc175440a569768aac0a26c5fc714f5fe
orez <yorez@naver.com>
2018-10-06 23:47:51 +0900

    express-session의 이해2

--- express-session.js
+++ express-session.js
@@ -11,9 +11,17 @@ app.use(session({
 }))
 
 app.get('/', function (req, res, next) {
-    res.send('Hello session')
+
+    console.log(req.session);
+    if (req.session.num === undefined) {
+        req.session.num = 1;
+    } else {
+        req.session.num = req.session.num + 1;
+    }
+    res.send(`Views : ${req.session.num}`);
+
 })
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('3000!');
 });
