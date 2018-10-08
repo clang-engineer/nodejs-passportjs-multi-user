44a84c34c96ebaac02eaf9c0b0cdf2ff774b3c00
orez <yorez@naver.com>
2018-10-08 18:50:28 +0900

    로그인, 인증 상태 UI에 반영

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var template = require('../lib/template');
+var auth = require('../lib/auth');
 
 router.get('/', function (request, response) {
     var title = 'WELCOME';
@@ -11,7 +12,7 @@ router.get('/', function (request, response) {
         ${description}
         <img src="/images/light.jpg" style="width:300px; display:block; margin-top:10px;">
         `,
-        `<a href="/topic/create">CREATE</a>`);
+        `<a href="/topic/create">CREATE</a>`,auth.statusUI(request,response));
     response.send(html);
 });
 
