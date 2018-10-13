dcf995ce58ccc8b123a551138cc8e280101c3d47
orez <yorez@naver.com>
2018-10-13 14:00:30 +0900

    passport 자격확인1

--- main.js
+++ main.js
@@ -39,6 +39,37 @@ app.use(session({
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function (username, password, done) {
+        console.log('LocalStrategy', username, password);
+        /*
+        User.findOne({
+          username: username
+        }, function (err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, {
+              message: 'Incorrect username.'
+            });
+          }
+          if (!user.validPassword(password)) {
+            return done(null, false, {
+              message: 'Incorrect password.'
+            });
+          }
+          return done(null, user);
+        });
+        */
+    }
+));
+
+
 app.post('/auth/login_process',
     passport.authenticate('local', {
         successRedirect: '/',
